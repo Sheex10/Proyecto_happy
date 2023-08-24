09808ba2c891105ca4ab5c60584fa9ab959ef806
Felipe <feliperivas1709@gmail.com>
2023-08-23 21:45:36 -0400

    xd
    xd

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'happy',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
